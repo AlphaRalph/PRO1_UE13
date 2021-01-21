15df8e9d80d7da19f8c0050451d025e9bf76186e
Ralph Slabihoud <ralph.slabihoud@lazersoft.at>
2021-01-21 10:40:25 +0100

    initial commit

---
+++ PRO1_UE13_git_project/PRO1_UE13_git_project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRO1_UE13_git_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
