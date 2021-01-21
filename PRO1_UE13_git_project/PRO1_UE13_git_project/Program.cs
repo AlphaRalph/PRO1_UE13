545180c470f7f6480dedcbdc93a30843f20624e2
Ralph Slabihoud <ralph.slabihoud@lazersoft.at>
2021-01-21 10:47:44 +0100

    improved usability

--- PRO1_UE13_git_project/PRO1_UE13_git_project/Program.cs
+++ PRO1_UE13_git_project/PRO1_UE13_git_project/Program.cs
@@ -10,7 +10,7 @@ namespace PRO1_UE13_git_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World");
         }
     }
 }
